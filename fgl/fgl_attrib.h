b623e7de751e78a192702def53599ebb8e0a28c1
Flatmush@googlemail.com <Flatmush@googlemail.com@5091b6ce-95b0-da31-bdb1-3f45d0d75d89>
2011-06-14 19:57:17 +0000

    Reverted attrib consts to defines since compiler complains that they aren't constants in the switch statement (dumb compiler).
    git-svn-id: https://fgl.googlecode.com/svn/trunk@15 5091b6ce-95b0-da31-bdb1-3f45d0d75d89

--- fgl/fgl_attrib.h
+++ fgl/fgl_attrib.h
@@ -9,13 +9,13 @@ extern "C"
 #include <stdbool.h>
 #include <stdint.h>
 
-static const uint8_t FGL_TEXTURE_2D          = 1;
-static const uint8_t FGL_DEPTH_TEST          = 2;
-static const uint8_t FGL_DEPTH_WRITE         = 3;
-static const uint8_t FGL_TRANSFORM           = 4;
-static const uint8_t FGL_CLIP                = 5;
-static const uint8_t FGL_CULL_FACE           = 6; // TODO
-static const uint8_t FGL_TEXTURE_PERSPECTIVE = 7; // TODO
+#define FGL_TEXTURE_2D          1
+#define FGL_DEPTH_TEST          2
+#define FGL_DEPTH_WRITE         3
+#define FGL_TRANSFORM           4
+#define FGL_CLIP                5
+#define FGL_CULL_FACE           6 // TODO
+#define FGL_TEXTURE_PERSPECTIVE 7 // TODO
 
 extern bool fgl_enable(uint8_t inCap);
 extern bool fgl_disable(uint8_t inCap);
